43de1fb99eddb19a3b76425b2a64cfea387be9ae
PEDROBELTRAN001 <145783001+PEDROBELTRAN001@users.noreply.github.com>
2026-03-03 17:48:13 -0400

    nombre

---
+++ src/app/components/avatar/avatar.ts
@@ -0,0 +1,37 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './avatar.html',
+  styleUrl: './avatar.scss',
+})
+export class AvatarComponent {
+  nombre = signal<string>('Avatar');
+  colorFondo = signal<string>('#fff');
+  tamano = signal<number>(150);
+  tieneLentes = signal<boolean>(false);
+  tipoExpresion = signal<number>(1);
+
+  cambiarNombre(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.nombre.set(input.value);
+  }
+
+  cambiarColor(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.colorFondo.set(input.value);
+  }
+  cambiarTamano(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tamano.set(parseInt(input.value));
+  }
+  toggleLentes() {
+    this.tieneLentes.update(flag => !flag); //es una funcion que recibe el valor actual y devuelve el valor actual negado
+  }
+  setExpresion(tipo: number) {
+    this.tipoExpresion.set(tipo);
+  }
+}
